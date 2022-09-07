8996f674e22b3e492a0f66b5d79d67c583c1fabf
tcm390 <60634884+tcm390@users.noreply.github.com>
2022-09-07 15:54:47 -0400

    Add files via upload

--- test-light/index.js
+++ test-light/index.js
@@ -3,17 +3,19 @@ import * as THREE from 'three';
 import metaversefile from 'metaversefile';
 import {WebaverseShaderMaterial} from '../../materials.js';
 import Simplex from '../../simplex-noise.js';
-const {useApp, useFrame, useInternals, useLoaders, usePhysics} = metaversefile;
+const {useApp, useFrame, useInternals, useLoaders, usePhysics, useLocalPlayer} = metaversefile;
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
 const simplex = new Simplex();
 const textureLoader = new THREE.TextureLoader();
 const lightTexture = textureLoader.load(`${baseUrl}/textures/light.png`);
-const maskTexture = textureLoader.load(`${baseUrl}/textures/mask3.png`);
+const maskTexture = textureLoader.load(`${baseUrl}/textures/mask5.png`);
+const trapezoidTexture = textureLoader.load(`${baseUrl}/textures/trapezoid.png`);
 
 export default () => {
   const app = useApp();
   const {camera} = useInternals();
+  const localPlayer = useLocalPlayer();
   const _getGeometry = (geometry, attributeSpecs, particleCount) => {
     const geometry2 = new THREE.BufferGeometry();
     ['position', 'normal', 'uv'].forEach(k => {
@@ -37,47 +39,40 @@ export default () => {
     return geometry2;
   };
   {
-    const particleCount = 30;
+    const particleCount = 20;
     const info = {
         fadeIn: [particleCount],
         maxOP: [particleCount],
     }
     const attributeSpecs = [];
-    attributeSpecs.push({name: 'id', itemSize: 1});
     attributeSpecs.push({name: 'scales', itemSize: 1});
     attributeSpecs.push({name: 'opacity', itemSize: 1});
     attributeSpecs.push({name: 'rotation', itemSize: 3});
-    const geometry2 = new THREE.PlaneBufferGeometry(0.3, 3.);
+    const geometry2 = new THREE.PlaneBufferGeometry(0.25, 1.2);
     const geometry = _getGeometry(geometry2, attributeSpecs, particleCount);
-    const idAttribute = geometry.getAttribute('id');
+    const scAttribute = geometry.getAttribute('scales');
+    const opAttribute = geometry.getAttribute('opacity');
     for(let i = 0; i < particleCount; i++){
-        idAttribute.setX(i, i);
+        scAttribute.setX(i, 0.8 + Math.random() * 0.5);
+        opAttribute.setX(i, Math.random());
         info.fadeIn[i] = true;
         info.maxOP[i] = 0.5 + Math.random() * 0.5;
     }
-    idAttribute.needsUpdate = true;
-    const opAttribute = geometry.getAttribute('opacity');
-    for(let i = 0; i < particleCount; i++){
-        opAttribute.setX(i, Math.random());
-    }
+    scAttribute.needsUpdate = true;
     opAttribute.needsUpdate = true;
 
     const material = new THREE.ShaderMaterial({
         uniforms: {
-            uTime: { value: 0 },
-            // opacity: { value: 0 },
             maskTexture: {
                 value: maskTexture
+            },
+            trapezoidTexture: {
+                value: trapezoidTexture
             }
         },
         vertexShader: `
             ${THREE.ShaderChunk.common}
             ${THREE.ShaderChunk.logdepthbuf_pars_vertex}
-            // uniform float uTime;
-            // uniform float size;
-            // uniform vec4 cameraBillboardQuaternion;
-            
-            attribute float id;
             attribute float scales;
             attribute float opacity;
             attribute vec3 rotation;
@@ -85,9 +80,8 @@ export default () => {
 
             varying vec2 vUv;
             varying float vOpacity;
-            // varying float vId;
-            
-            
+            varying vec3 vPos;
+           
             void main() {  
                 mat3 rotX = mat3(
                     1.0, 0.0, 0.0, 
@@ -106,9 +100,9 @@ export default () => {
                 );
                 vOpacity = opacity;
                 vUv = uv;
-                // vId = id;
+                vPos = position;
                 vec3 pos = position;
-                pos *= scales;
+                pos.x *= scales;
                 pos *= rotY;
                 pos *= rotZ;
                 pos *= rotX;
@@ -124,37 +118,28 @@ export default () => {
         `,
         fragmentShader: `
             ${THREE.ShaderChunk.logdepthbuf_pars_fragment}
-            // uniform float uTime;
-            // uniform float sphereNum;
-            // uniform float opacity;
-
             uniform sampler2D maskTexture;
+            uniform sampler2D trapezoidTexture;
             
             varying vec2 vUv;
             varying float vOpacity;
-            // varying float vId;
-            // varying float vTextureRotation;
-
-            // #define PI 3.1415926
-
+            varying vec3 vPos;
 
             void main() {
                 
-                // vec4 tex = texture2D(electronicballTexture, vUv); 
-                // if(tex.a < 0.01)
-                // {
-                //     discard;    
-                // }   
-                // gl_FragColor.r *= abs(sin(uTime) * vId);
-                // gl_FragColor.g *= abs(sin(uTime) * vId);
-                // gl_FragColor = tex;
-                // gl_FragColor.a *= opacity;
                 vec4 mask = texture2D(maskTexture, vUv); 
-                float s = smoothstep(0.5, 0.2, length(vUv - 0.5));
-                gl_FragColor = vec4(s) * vec4(0.120, 0.280, 1.920, 1.0) * (2. + vUv.y);
+                vec4 trapezoid = texture2D(trapezoidTexture, vUv); 
                 
-                gl_FragColor.a = vOpacity * vUv.y * vUv.y * vUv.y;
-                // gl_FragColor *= mask;
+                // scanline
+                float scanline = sin(vPos.y * 160.0) * 0.04;
+                
+                
+                gl_FragColor = trapezoid * vec4(0.120, 0.280, 1.920, 1.0) * (1. + vUv.y);
+                
+                
+                if (gl_FragColor.b > 0.)
+                    gl_FragColor -= scanline;
+                gl_FragColor.a = vOpacity * pow(vUv.y, 1.0);
                 
                 ${THREE.ShaderChunk.logdepthbuf_fragment}
                 
@@ -166,22 +151,27 @@ export default () => {
         blending: THREE.AdditiveBlending,
     });
     const godRay = new THREE.InstancedMesh(geometry, material, particleCount);
-    app.add(godRay)
+    const group = new THREE.Group();
+    group.add(godRay);
+    app.add(group)
+    group.rotation.x = -Math.PI;
+    // group.position.y = 4.3;
     
     useFrame(({timestamp}) => {
+        group.position.copy(localPlayer.position);
+        group.position.y += localPlayer.avatar ? localPlayer.avatar.height - 0.5 : 0;
         const scalesAttribute = godRay.geometry.getAttribute('scales');
         const positionsAttribute = godRay.geometry.getAttribute('positions');
         const rotationAttribute = godRay.geometry.getAttribute('rotation');
         const opacityAttribute = godRay.geometry.getAttribute('opacity');
-        const IDAttribute = godRay.geometry.getAttribute('id');
         
         for(let i = 0; i < particleCount; i++){
             if (opacityAttribute.getX(i) <= 0) {
                 rotationAttribute.setY(i, Math.random() * 2 * Math.PI);
                 opacityAttribute.setX(i, Math.random() * 0.05);
-                IDAttribute.setX(i, Math.floor(Math.random() * particleCount));
                 info.maxOP[i] = 0.5 + Math.random() * 0.5;
                 info.fadeIn[i] = true;
+                scalesAttribute.setX(i, 0.8 + Math.random());
             }
 
             if (opacityAttribute.getX(i) >= info.maxOP[i]) {
@@ -190,37 +180,39 @@ export default () => {
 
 
             if (!info.fadeIn[i]) {
-                opacityAttribute.setX(i, opacityAttribute.getX(i) - 0.0025);
+                opacityAttribute.setX(i, opacityAttribute.getX(i) - 0.005);
             }
             else {
-                opacityAttribute.setX(i, opacityAttribute.getX(i) + 0.0025);
+                opacityAttribute.setX(i, opacityAttribute.getX(i) + 0.005);
             }
             
-            scalesAttribute.setX(i, 1);
+            
             const theta = 2. * Math.PI * i / particleCount;
-            let xNoise = simplex.noise1D(Math.sin(theta) * timestamp * 0.001) * 0.2;
-            let zNoise = simplex.noise1D(Math.cos(theta) * timestamp * 0.001) * 0.2;
+            let xNoise = simplex.noise1D(Math.sin(theta) * timestamp * 0.001) * 0.05;
+            let zNoise = simplex.noise1D(Math.cos(theta) * timestamp * 0.001) * 0.05;
             positionsAttribute.setXYZ(
                                     i,
-                                    Math.sin(theta) * 0.65 + xNoise,
-                                    3,
-                                    Math.cos(theta) * 0.65 + zNoise
+                                    Math.sin(theta) * 0.22 + xNoise,
+                                    0,
+                                    Math.cos(theta) * 0.22 + zNoise
             ) 
+            // const n = Math.cos(theta) > 0 ? 1 : -1;
+            const n = Math.cos(camera.rotation.y) > 0 ? -1 : 1;
+            const angle = Math.PI / 8;
             rotationAttribute.setXYZ(
                             i,
-                            Math.PI / 6 * Math.cos(theta),
-                            rotationAttribute.getY(i),
-                            Math.PI / 6 * -Math.sin(theta)
-            )  
+                            angle * Math.cos(theta),
+                            // -Math.sin(theta) * n * (Math.PI / 2),
+                            // rotationAttribute.getY(i),
+                            -Math.sin(camera.rotation.y) * n * (Math.PI / 2),
+                            angle * -Math.sin(theta)
+            ) 
         }
         
         scalesAttribute.needsUpdate = true;
         positionsAttribute.needsUpdate = true;
         rotationAttribute.needsUpdate = true;
         opacityAttribute.needsUpdate = true;
-        IDAttribute.needsUpdate = true;
-        // material.uniforms.uTime.value = timestamp / 1000;
-        // material.uniforms.cameraBillboardQuaternion.value.copy(camera.quaternion);
     
         app.updateMatrixWorld();
     });
